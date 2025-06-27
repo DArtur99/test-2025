2a95d43b7de1d96bd2c295b647d656cb4aa0fd5b
Artur Davtyan <dartcor.ru_01@mail.ru>
2025-06-27 16:09:16 +0300

    task_2 updated: added functions for statistics

--- textstat/index.js
+++ textstat/index.js
@@ -10,6 +10,42 @@
 function onStatisticsClicked(event) {
     //TODO: Implement this function
     console.log('onStatisticsClicked called');
+
+    const text = document.getElementById('text').value;
+    // 1) Characters count
+    const charCount = text.length;
+
+    // 2) Spaces count
+    let spacesCount = 0;
+    for (let char of text) {
+        if (char === ' ') {
+            spacesCount++;
+        }
+    }
+
+    // 3) Vowels count and Consonants count *only english letters
+    let vowelsCount = 0;
+    let consonantsCount = 0;
+    const vowels = 'aeiou';
+    const consonants = 'bcdfghjklmnpqrstvwxyz';
+    
+    for (const char of text) {
+        if (vowels.includes(char.toLowerCase())) {
+            vowelsCount++;
+        } else if (consonants.includes(char)) {
+            consonantsCount++;
+        }
+    }
+
+    // 4) Words count
+    const wordsArray = text.trim() === "" ? [] : text.trim().split(/\s+/);
+    const wordsCount = wordsArray.length;
+
+    document.getElementById('valCharCount').value = charCount;
+    document.getElementById('valSpacesCount').value = spacesCount;
+    document.getElementById('valVowelsCount').value = vowelsCount;
+    document.getElementById('valConsonantsCount').value = consonantsCount;
+    document.getElementById('valWordsCount').value = wordsCount;
 }
 
 /**
@@ -20,6 +56,14 @@ function onStatisticsClicked(event) {
 function onRemoveWordsClicked(event) {
     //TODO: Implement this function
     console.log('onRemoveWordsClicked called');
+
+    const text = document.getElementById('text').value;
+    const words = text.trim().split(/\s+/);
+    
+    const oddWords = words.filter((word, index) => index % 2 === 0);
+    const oddWordsResult = oddWords.join(' ');
+
+    document.getElementById('valOddWords').value = oddWordsResult;
 }
 
 
@@ -28,6 +72,9 @@ function onRemoveWordsClicked(event) {
 function init() {
     const text = document.getElementById('text');
     text.value = txt;
+    // Назначаем обработчики кнопкам
+    document.getElementById('statistics').addEventListener('click', onStatisticsClicked);
+    document.getElementById('removewords').addEventListener('click', onRemoveWordsClicked);
 }
 
 document.addEventListener('DOMContentLoaded', init);
